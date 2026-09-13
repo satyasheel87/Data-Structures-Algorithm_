1c17e5ae51e92858c2f0cb9c88837a2d016164db
satyasheel gatuam <satyasheelgautam87@gmial.com>
2026-09-13 13:58:28 +0530

    trapping rain water

--- 1- practice-js/DSA_Practice.js
+++ 1- practice-js/DSA_Practice.js
@@ -5,31 +5,27 @@
 
 
 // Trapping rain water 
-let height = [4, 2, 0, 3, 2, 5];
+let height = [4, 2, 0, 3, 2, 5]
+ 
+let left = new Array(height.length)
+let right = new Array(height.length)
 
-let left = new Array(height.length);
-let right = new Array(height.length);
-let maxLeft = height[0]; //4
-let maxRight = height[height.length - 1]; //5
+let maxLeft = height[0]
+let maxRight = height[height.length - 1]
 
-left[0] = maxLeft;
-right[right.length - 1] = maxRight;
-
-for (let i = 1; i < height.length; i++) {
-  maxLeft = Math.max(height[i], maxLeft);
-  left[i] = maxLeft;
+for (let i = 0; i < height.length; i++) {
+  maxLeft = Math.max(maxLeft, height[i])
+  left[i] = maxLeft
 }
-for (let i = height.length - 2; i >= 0; i--) {
-  maxRight = Math.max(height[i], maxRight);
-  right[i] = maxRight;
+ 
+for (let i = height.length - 1; i >= 0; i--) {
+  maxRight = Math.max(maxRight, height[i])
+  right[i] = maxRight
 }
-console.log(left); // [4, 4, 4, 4, 4, 5];
-console.log(right); // [5, 5, 5, 5, 5, 5];
 
 let ans = 0;
 for (let i = 0; i < height.length; i++) {
-  ans += Math.min(left[i], right[i]) - height[i];
+  let minimum = Math.min(left[i], right[i])
+  ans += minimum - height[i]
 }
-
-console.log(ans);
-
+console.log(ans)
